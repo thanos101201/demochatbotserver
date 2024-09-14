c11f0fba1aa0c1dade25faac5929419f3f77d2aa
thanos101201 <ayushsingh10122001@gmail.com>
2024-09-14 15:18:27 +0530

    formatted the code.

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json);
-app.use(cors({
-    origin: '*',
-    methods: [
-        'GET',
-        'PUT',
-        'POST',
-        'DELETE'
-    ]
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+  })
+);
 
 app.listen(3001, () => {
-    console.log("Server started at port 3001."); 
+  console.log("Server started at port 3001.");
 });
